c4a0b4e8b0cf1f15de8b9be5a3f05ff0b11fa045
SebasCCR <sebastianccrojas@gmail.com>
2025-09-12 22:08:31 -0500

    borar todosss

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HeaderComponents from './components/HeaderComponents'
-import LoginPage from './components/LoginPage'
-import RegisterPage from './components/RegisterPage'
-import UserProfile from './components/UserProfile'
-import FooterComponents from './components/FooterComponents'
-import Muro from './components/Muro'
+import interfaces from './interfaces/inicio'
+
 
 function App() {
   return (
     <Router>
-      <HeaderComponents />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/profile" element={<UserProfile />} />
-         <Route path="/muro" element={<Muro />} />   {/* 👈 Nueva ruta */}
+        <Route path="/" element={<inicio />} />
       </Routes>
-      <FooterComponents />
     </Router>
   )
 }
