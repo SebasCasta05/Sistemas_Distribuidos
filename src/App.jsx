a1986d973ada727282cf393607fca9444a5965fe
Andres0606 <andres.suarezro@campusucc.edu.co>
2025-09-12 22:48:14 -0500

    agregado inicio en app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<inicio />} />
+        <Route path="/inicio" element={<inicio />} />
       </Routes>
     </Router>
   )
